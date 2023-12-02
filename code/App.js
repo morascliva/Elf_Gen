6b1e67a5328b0828171f2f83fd3c7a466198d5c3
Cliva Moras <94843082+morascliva@users.noreply.github.com>
2023-12-02 21:08:12 +0530

    Create App.js

---
+++ code/App.js
@@ -0,0 +1,19 @@
+// App.js
+import React from 'react';
+import './App.css';
+import './index.css'; 
+import ElfNameGenerator from './ElfNameGenerator';
+
+const App = () => {
+  return (
+    <div>
+      <h1>
+        <break>
+        </break>
+      </h1>
+      <ElfNameGenerator />
+    </div>
+  );
+};
+
+export default App;
